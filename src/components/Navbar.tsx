04107774dc3af0b9b62b1f1c5f9d0ffb36567902
nihalmohammed18 <nihalmohammed79@gmail.com>
2023-07-16 20:27:38 +0530

    Create User Profile Dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
   const session = await getAuthSession();
@@ -19,8 +20,8 @@ const Navbar = async () => {
 
         {/* Search Bar */}
 
-        {session ? (
-          <p>You are Logged In</p>
+        {session?.user ? (
+          <UserAccountNav user={session.user} />
         ) : (
           <Link href="/sign-in" className={buttonVariants()}>
             Sign In
